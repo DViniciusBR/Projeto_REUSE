a9c75b876c95d8539f7df58163a06d5a2edb1ba4
Re-Use2990 <101225745+Re-Use2990@users.noreply.github.com>
2022-08-10 15:15:39 -0300

    Delete Tela2.js

--- Tela2.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, ImageBackground, SafeAreaView, Button } from 'react-native';
-
-export default function TelaInicial() {
-  return <Text>Pagina 1</Text>
-}
